f6cd7259821210b1e99b26c1bef09a952c9f9524
abicelaya <abicelaya@gmail.com>
2022-10-07 11:12:26 +0000

    generador terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+
+  var one = Math.floor(Math.random() * pronoun.length);
+  var two = Math.floor(Math.random() * adj.length);
+  var three = Math.floor(Math.random() * noun.length);
+
+  console.log(pronoun[one] + adj[two] + noun[three] + ".com");
 };
